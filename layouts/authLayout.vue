56601a50744f7cb0d69223ee65c0965ed555bd48
Favour Felix <felixfavour0@gmail.com>
2022-01-15 19:06:23 +0400

    feat: added login screen markup

---
+++ layouts/authLayout.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="auth-ctn page">
+    <div class="top-bg section">
+      <div class="inner">
+        <div class="text-1">
+          We're making sure you <br>
+          <span>never settle</span> for less.
+        </div>
+        <div class="text-2">
+          Find the best employees and employers <br>
+          Be the best employee or employer
+        </div>
+        <div v-show="$route.name.includes('signup')" class="actions come-down-sm">
+          <button class="primary-btn uni-btn">
+            University
+          </button>
+          <button class="primary-btn employer-btn">
+            Employer
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="bottom-bg bg-img school-login" />
+    <div class="page-content">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AuthLayout',
+  layout: 'noAuth'
+}
+</script>
+
+<style scoped>
+  .top-bg {
+    background: var(--primary);
+    height: 355px;
+    padding-top: 4rem;
+  }
+  .bottom-bg {
+    height: calc(100vh - 355px);
+    background-image: url(~assets/images/school-login-bg.png);
+  }
+  .page-content {
+    position: absolute;
+    max-width: 500px;
+    top: 4rem;
+    right: 10%;
+  }
+  .text-1 {
+    font-size: 2.2rem;
+    color: #FFFFFF;
+    line-height: 3rem;
+    margin-bottom: 12px;
+    font-weight: 700;
+  }
+  .text-1 > span {
+    color: var(--primary-dark);
+    font-weight: inherit;
+  }
+  .text-2 {
+    font-size: 1.2rem;
+    color: #FFFFFF;
+    line-height: 2rem;
+    font-weight: 400;
+  }
+  .top-bg .actions {
+    display: flex;
+    margin-top: 20px;
+  }
+  .uni-btn {
+    background: #FFFFFF;
+    color: var(--primary);
+    border: 1px solid #FFFFFF;
+  }
+  .employer-btn {
+    background: var(--primary);
+    color: #FFFFFF;
+    border: 1px solid #FFFFFF;
+    margin-left: 12px;
+  }
+</style>
---
+++ pages/_user/login.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="login-ctn">
+    <div class="header">
+      <div class="logos">
+        <div class="uni-logo bg-img reveals" />
+        +
+        <div class="ei-logo bg-img reveals" />
+      </div>
+      <div class="large-text">
+        <LargeLogo />
+        <div>for Middlesex University</div>
+      </div>
+    </div>
+    <form @submit.prevent="">
+      <div class="form-group">
+        <input type="email">
+        <label for="email">
+          University Email
+        </label>
+      </div>
+      <div class="form-group">
+        <input type="password">
+        <label for="password">
+          Password
+        </label>
+      </div>
+      <button class="primary-btn">
+        SIGN IN
+      </button>
+      <div class="signup-text">
+        <span>Don't have an account?</span>
+        <nuxt-link to="/signup">
+          Sign up
+        </nuxt-link>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+  layout: 'authLayout'
+}
+</script>
+
+<style scoped>
+  .top-bg {
+    background: var(--primary);
+    height: 355px;
+  }
+  .login-ctn {
+    width: 500px;
+  }
+
+  /* HEADER */
+  .header {
+    display: flex;
+    margin-bottom: 5%;
+  }
+  .logos {
+    font-weight: 800;
+    color: #FFFFFF;
+    font-size: 1.3rem;
+    display: flex;
+    align-items: center;
+  }
+  .uni-logo, .ei-logo {
+    width: 50px;
+    height: 50px;
+    border-radius: 8px;
+  }
+  .uni-logo {
+    background-image: url(~assets/images/sample-school.png);
+    margin-right: 8px;
+  }
+  .ei-logo {
+    background-image: url(~assets/images/ei-logo.png);
+    margin-left: 8px;
+  }
+  .large-text {
+    padding-top: 40px;
+    margin-left: 18px;
+  }
+  .large-text > div {
+    font-size: 18px;
+    line-height: 2rem;
+    font-weight: 400;
+    color: #FFFFFF;
+  }
+
+  /* FORM */
+  form {
+    width: 100%;
+    background: #FFFFFF;
+    padding: 32px 48px;
+    border-radius: 8px;
+  }
+  .form-group:nth-child(1) {
+    margin-top: 8px;
+  }
+  .primary-btn {
+    margin-top: 5rem;
+    width: 100%;
+  }
+  .signup-text {
+    margin: 1rem 0;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 0.875rem;
+    color: var(--primary);
+  }
+  .signup-text a {
+    font-size: inherit;
+    color: inherit;
+    font-weight: 500;
+    margin-left: 4px;
+    text-decoration: underline;
+  }
+</style>
