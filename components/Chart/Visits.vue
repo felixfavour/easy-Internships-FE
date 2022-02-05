0663797a9a3b5949c4518dcbeac109c227c20f2e
Favour Felix <felixfavour0@gmail.com>
2022-02-05 22:41:40 +0400

    finished performance index screen

---
+++ components/Chart/Visits.vue
@@ -0,0 +1,89 @@
+<template>
+  <canvas ref="chart" />
+</template>
+
+<script>
+import { Chart, registerables } from 'chart.js'
+Chart.register(...registerables)
+export default {
+  name: 'VisitsChart',
+  props: {
+    chartData: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data () {
+    return {
+      labels: ['Jan 1', 'Jan 2', 'Jan 3', 'Jan 4', 'Jan 5', 'Jan 6', 'Jan 7'],
+      dataSets: [10, 32, 23, 54, 45, 33, 70]
+    }
+  },
+  mounted () {
+    this.mountChart()
+  },
+  methods: {
+    mountChart () {
+      // eslint-disable-next-line no-unused-vars
+      const myChart = new Chart(this.$refs.chart, {
+        type: 'bar',
+        data: {
+          labels: this.labels,
+          datasets: [
+            {
+              label: [''],
+              data: this.dataSets,
+              backgroundColor: [
+                '#01058A',
+                '#01058A',
+                '#01058A'
+              ],
+              borderColor: [
+                '#01058A',
+                '#01058A',
+                '#01058A'
+              ]
+            }
+          ]
+        },
+        options: {
+          scales: {
+            x: {
+              grid: {
+                display: false
+              },
+              plugins: {
+                datalabels: {
+                  display: false
+                }
+              }
+            },
+            y: {
+              beginAtZero: true,
+              ticks: {
+                precision: 0
+              }
+            }
+          }
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+  canvas {
+    margin-top: 1rem;
+    background: #FFFFFF;
+    padding: 2rem;
+    box-shadow: 0px 4px 17px 1px rgba(0, 123, 236, 0.24);
+    border-radius: 6px;
+  }
+
+  /* MEDIA QUERIES */
+  @media screen and (max-width: 600px) {
+    canvas {
+    }
+  }
+</style>
--- pages/performance/index.vue
+++ pages/performance/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="performance-section">
+  <div class="performance-section page">
     <PageBanner
       :data="{
         sub: 'See how employers see you',
@@ -14,6 +14,22 @@
           <PerformanceBriefCard :performance="{ label: 'Unique Visits', value:'47' }" />
           <PerformanceBriefCard :performance="{ label: 'Academic Score', value:'85%' }" />
           <PerformanceBriefCard :performance="{ label: 'Skills Score', value:'93%' }" />
+        </div>
+        <div class="chart-ctn">
+          <div class="chart-header">
+            <div class="text">
+              Number of Visits
+            </div>
+            <div class="form-group">
+              <select id="location" name="location">
+                <option value="1-week">
+                  Last 1 Week
+                </option>
+              </select>
+              <IconArrowDown class="icon" />
+            </div>
+          </div>
+          <ChartVisits />
         </div>
       </div>
     </div>
@@ -32,5 +48,19 @@ export default {
     display: grid;
     grid-template-columns: repeat(4, 24.5%);
     column-gap: 1.5%;
+    margin-bottom: 1.5rem;
+  }
+  .chart-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 1.25rem;
+    font-weight: 600;
+  }
+  .chart-header .form-group {
+    width: 250px;
+  }
+  .form-group .icon {
+    top: 15px;
   }
 </style>
