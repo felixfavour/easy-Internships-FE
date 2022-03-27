48695ef112c0244b6986fb3614f341c71119ac35
Favour Felix <favour@acumen.digital>
2022-03-27 20:11:47 +0400

    added filter logic to reviews

--- pages/employer/_employer_id/reviews.vue
+++ pages/employer/_employer_id/reviews.vue
@@ -9,8 +9,13 @@
               + Add Your Review
             </button>
           </div>
+          <div class="row row-3">
+            <button v-for="tag in tags" :key="tag" :class="['chip-btn', selectedTags.includes(tag) ? 'active' : '']" @click="addTag(tag)">
+              {{ tag }}
+            </button>
+          </div>
           <div class="reviews-list">
-            <ReviewCard v-for="review in reviews" :key="review._id" :review="review" />
+            <ReviewCard v-for="review in reviewsCopy" :key="review._id" :review="review" />
           </div>
         </section>
       </div>
@@ -35,7 +40,36 @@ export default {
   },
   data () {
     return {
-      addReviewVisible: false
+      addReviewVisible: false,
+      selectedTags: [],
+      reviewsCopy: [...this.reviews],
+      tags: [...this.$store.state.reviewTags]
+    }
+  },
+  watch: {
+    selectedTags (val) {
+      if (val.length === 0) {
+        this.reviewsCopy = [...this.reviews]
+      } else {
+        this.reviewsCopy = []
+        for (const review of this.reviews) {
+          for (const tag of this.selectedTags) {
+            if (review.tags.includes(tag)) {
+              this.reviewsCopy.push(review)
+              break
+            }
+          }
+        }
+      }
+    }
+  },
+  methods: {
+    addTag (tag) {
+      if (this.selectedTags.includes(tag)) {
+        this.selectedTags = this.selectedTags.filter(t => t !== tag)
+      } else {
+        this.selectedTags.push(tag)
+      }
     }
   }
 }
@@ -56,5 +90,27 @@ export default {
   }
   .clear-btn {
     color: var(--primary);
+  }
+
+  /* ROW 3 */
+  .row-3 {
+    display: flex;
+    flex-wrap: wrap;
+    margin-bottom: 24px;
+  }
+  .chip-btn {
+    margin-right: 12px;
+    height: 30px;
+    display: grid;
+    place-items: center;
+    padding: 6px 12px;
+    background: #FFFFFF;
+    border: 1px solid var(--primary-dark);
+    border-radius: 30px;
+    color: var(--primary-dark);
+  }
+  .chip-btn:active, .chip-btn.active {
+    background: var(--primary-dark);
+    color: #FFFFFF;
   }
 </style>
